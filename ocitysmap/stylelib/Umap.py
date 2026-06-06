63eddcf60bf9b24d6270966e96ea664ff43ee41d
Hartmut Holzgraefe <hartmut@php.net>
2026-06-06 23:47:12 +0000

    fix E712 comparison to True should be 'if cond is True:' or 'if cond:'

--- ocitysmap/stylelib/Umap.py
+++ ocitysmap/stylelib/Umap.py
@@ -82,7 +82,7 @@ def get_default_properties(json, umap_defaults, create_copy=True):
     for path in ['$.properties.*', '$.properties._storage.*', '$._storage.*', '$.properties._storage_options.*', '$._storage_options.*', '$.properties._umap_options.*', '$._umap_options.*']:
         for key,value in flattened(json, path).items():
             if key in ['name', 'opacity', 'fillOpacity', 'weight', 'dashArray', 'iconClass', 'iconUrl']:
-                if value == True:
+                if value is True:
                     value = 'yes'
                 values[key] = value
             elif key in ['color', 'fillColor']:
