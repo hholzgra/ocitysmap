a0a63d064f91dcee25c8946a03dda7a41bb0c31d
Hartmut Holzgraefe <hartmut@php.net>
2026-06-07 23:38:00 +0000

    whitespace

--- ocitysmap/indexlib/renderer.py
+++ ocitysmap/indexlib/renderer.py
@@ -70,6 +70,7 @@ if __name__ == '__main__':
     import random
     import string
 
+    import ocitysmap
     from GeneralIndex import GeneralIndexCategory, GeneralIndexTerm, GeneralIndexRenderer
 
     logging.basicConfig(level=logging.DEBUG)
